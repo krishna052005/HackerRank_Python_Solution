d50a85588f93aea4bcd900db3e8d8a06151361a8
Krishna Vora <kpvora05@gmail.com>
2026-01-29 20:38:43 +0530

    Add files via upload

---
+++ Introduction/Problem_5.py
@@ -0,0 +1,9 @@
+-- Problem: Loops
+--Platform: HackerRank
+--Difficulty: Easy
+
+Question: The provided code stub reads an integer, n, from STDIN. For all non-negative integers i<n, print i^2.
+
+n=int(input())
+for i in range(n):
+    print(i*i)
