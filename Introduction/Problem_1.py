f13c5978b8e69946c7208504a16fa67a409455aa
Krishna Vora <kpvora05@gmail.com>
2026-01-29 20:33:31 +0530

    Add files via upload

---
+++ Introduction/Problem_1.py
@@ -0,0 +1,13 @@
+-- Problem: Say "Hello, World!" With Python
+--Platform: HackerRank
+--Difficulty: Easy
+
+Question: Here is a sample line of code that can be executed in Python:
+
+print("Hello, World!")
+You can just as easily store a string as a variable and then print it to stdout:
+my_string = "Hello, World!"
+print(my_string)
+The above code will print Hello, World! on your screen. Try it yourself in the editor below!
+
+print("Hello, World!")
