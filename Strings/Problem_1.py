c1b5780daeb5398b2b8ef60d8bac75ffdc52c454
Krishna Vora <kpvora05@gmail.com>
2026-01-31 22:27:23 +0530

    Add files via upload

--- Strings/Problem_1.py
+++ Strings/Problem_1.py
@@ -1,18 +1,14 @@
--- Problem: List Comprehensions
+-- Problem: sWAP cASE
 --Platform: HackerRank
 --Difficulty: Easy
 
-Question: Let's learn about list comprehensions! You are given three integers x,y and z representing the dimensions of a cuboid along with an integer n. Print a list of all possible coordinates given by (i,j,k) on a 3D grid where the sum of i+j+k is not equal to n. Here, 0<i<x; 0<7<y; 0<k<z. Please use list comprehensions rather than multiple loops, as a learning exercise.
+Question: You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
 
-x=int(input())
-y=int(input())
-z=int(input())
-n=int(input())
 
-result=[[i,j,k]
-for i in range(x+1)
-for j in range(y+1)
-for k in range(z+1)
-if i+j+k!=n
-]
-print(result)
+def swap_case(s):
+    return
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
